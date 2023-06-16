f328af8cd309e13a534fbce5687da7ec05668216
Ernest Cui <ecuiships@126.com>
2023-06-16 23:06:57 +0800

    chore: main page tab finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "antd/dist/reset.css";
+import styles from "./App.module.scss";
+
+import { Layout, Tabs } from "antd";
+import type { TabsProps } from "antd";
+
+const { Header, Content } = Layout;
+
+const tabItems: TabsProps["items"] = [
+    {
+        key: "0",
+        label: "输入",
+        children: `Content of Tab Pane 1`
+    },
+    {
+        key: "1",
+        label: "评价",
+        children: `Content of Tab Pane 2`
+    }
+];
 
 const App: React.FC = () => {
-    return <div></div>;
+    return (
+        <Layout className="layout">
+            <Header className={styles.header}>
+                <div className={styles.divHeaderText}>
+                    S3TD：基于语义表征的多源数据融合分析系统
+                </div>
+            </Header>
+            <Content className={styles.content}>
+                <Tabs defaultActiveKey="0" items={tabItems} />
+            </Content>
+        </Layout>
+    );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "rgb(132, 122, 244)"
+                }
+            }}>
+            <App />
+        </ConfigProvider>
     </React.StrictMode>
 );
