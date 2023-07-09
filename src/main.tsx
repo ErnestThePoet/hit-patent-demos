642c4442103df03a9f48d05930f09ae4fde72baf
Ernest Cui <ecuiships@126.com>
2023-07-09 19:00:48 +0800

    chore: move config provider to individual page

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: "rgb(132, 122, 244)"
-                }
-            }}>
-            <App />
-        </ConfigProvider>
+        <App />
     </React.StrictMode>
 );
--- src/pages/S3TD/S3TD.tsx
+++ src/pages/S3TD/S3TD.tsx
@@ -4,6 +4,7 @@ import { Layout, Tabs } from "antd";
 import type { TabsProps } from "antd";
 import DataInput from "./components/DataInput/DataInput";
 import Evaluation from "./components/Evaluation/Evaluation";
+import { ConfigProvider } from "antd";
 
 const { Header, Content } = Layout;
 
@@ -22,16 +23,23 @@ const tabItems: TabsProps["items"] = [
 
 const S3TD: React.FC = () => {
     return (
-        <Layout className="layout">
-            <Header className={styles.header}>
-                <div className={styles.divHeaderText}>
-                    S3TD：基于语义表征的多源数据融合分析系统
-                </div>
-            </Header>
-            <Content className={styles.content}>
-                <Tabs defaultActiveKey="0" items={tabItems} />
-            </Content>
-        </Layout>
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "rgb(132, 122, 244)"
+                }
+            }}>
+            <Layout className="layout">
+                <Header className={styles.header}>
+                    <div className={styles.divHeaderText}>
+                        S3TD：基于语义表征的多源数据融合分析系统
+                    </div>
+                </Header>
+                <Content className={styles.content}>
+                    <Tabs defaultActiveKey="0" items={tabItems} />
+                </Content>
+            </Layout>
+        </ConfigProvider>
     );
 };
 
